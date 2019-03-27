b711ac6109eea0fe5d5299fef30cccda503e1782
Christian Rittmann <crittmann@hotmail.com>
2019-03-26 23:27:38 -0500

    moved from other computer and updated

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+  <header className="header">
+    <h1>Clicky Game</h1>
+    <h2>
+      Click the images to earn points, but don't click an image more then once!
+    </h2>
+  </header>
+);
+
+export default Header;
